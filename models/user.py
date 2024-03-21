77e6930283fc9e3ef275bc34f0ea350bd2cab471
Abdelrahman Amr Sobh <AbdelrahmanAmr.Egypt@gmail.com>
2024-03-21 03:09:56 -0700

    Fix typo

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from sqlalchemy import String, Integer, Column
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = = "users"
+    __tablename__ = "users"
     email = Column(String(60), nullable=False)
     password = Column(String(60), nullable=False)
     first_name = Column(String(60), nullable=False)
