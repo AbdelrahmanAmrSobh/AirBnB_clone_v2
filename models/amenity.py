187b498dc1dc4082b569d1cdd39f957b0fa38403
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-06-27 00:47:49 +0300

    Fix bug don't allow null values for name of Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -10,7 +10,7 @@ from models.place import place_amenity
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     if file_storage_type == "db":
-        name = Column(String(128), nullable=True)
+        name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary=place_amenity)
     else:
         name = ""
