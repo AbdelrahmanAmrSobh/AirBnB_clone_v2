1bcf8589037c223050b25256beb09007c113388a
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-06-27 02:42:46 +0300

    move import only to be used when needed to avoid bugs

--- models/amenity.py
+++ models/amenity.py
@@ -4,12 +4,12 @@ from models.base_model import BaseModel, Base
 from models import file_storage_type
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     if file_storage_type == "db":
+        from models.place import place_amenity
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
     else:
