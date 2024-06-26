e572105d0f74e349a428a1be0c78ae03479dc19d
Abdelrahman Amr <AbdelrahmanAmr.Egypt@gmail.com>
2024-06-27 01:05:56 +0300

    Fix a bug

--- models/base_model.py
+++ models/base_model.py
@@ -22,11 +22,20 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            try:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            except KeyError:
+                pass
+            try:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+            except KeyError:
+                pass
+            try:
+                del kwargs['__class__']
+            except KeyError:
+                pass
             self.__dict__.update(kwargs)
 
     def __str__(self):
